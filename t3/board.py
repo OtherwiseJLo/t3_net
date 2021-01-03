95b24ca85dafd723cf8a960140358f74be2d83d2
OtherwiseJLo <jonathanglopez@gmail.com>
2021-01-03 09:59:50 -0500

    Updating project structure

--- t3/board.py
+++ t3/board.py
@@ -3,11 +3,13 @@ import numpy as np
 
 class Board:
     def __init__(self, state: str):
+        self.state = state
         self.board = np.array([int(i) for i in state], dtype=np.uint8)
         self.board.reshape((2, 3, 3))
+        self.label = self._calculate_label()
 
-    def get_state(self) -> str:
-        return self.state
+    def _calculate_label(self):
+        return "NULL"
 
     def _is_invalid(self) -> bool:
         if self.board.sum() < 5 or self.board > 9:
